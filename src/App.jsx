5582a52a2a32b2fcebe75699e538d877944271f5
MeiyinPU <mei-yin.pu@oclock.school>
2024-01-11 21:48:52 +0100

    modify text

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h2 style="color:red">Mei-Yin Go Go</h2>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -27,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Mei-Yin Go Go
       </p>
     </>
   )
