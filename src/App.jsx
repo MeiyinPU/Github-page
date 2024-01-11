bbfa81adcf31edc548cc8ddd59db691102eef9b3
MeiyinPU <mei-yin.pu@oclock.school>
2024-01-11 21:46:41 +0100

    add text h2

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <h2 style="color:red">Mei-Yin Go Go</h2>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
